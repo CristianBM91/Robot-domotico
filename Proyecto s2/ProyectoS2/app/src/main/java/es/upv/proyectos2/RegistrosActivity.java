173a7b0268bb43830546f8283561e46abbeec0b8
ruben021000 <55706771+ruben021000@users.noreply.github.com>
2019-11-15 12:03:06 +0100

    Add files via upload

---
+++ Proyecto s2/ProyectoS2/app/src/main/java/es/upv/proyectos2/RegistrosActivity.java
@@ -0,0 +1,14 @@
+package es.upv.proyectos2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registros);
+    }
+}
