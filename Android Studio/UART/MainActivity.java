adb44da44e93dfc7199b5be648b32bb3bf6e96de
MicheleDellamorte <david95999@hotmail.es>
2019-11-15 18:53:00 +0100

    Conexión UART con Android Things.

---
+++ Android Studio/UART/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidthingsuart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "Lista de UART disponibles: " + ArduinoUart.disponibles());
+        ArduinoUart uart = new ArduinoUart("UART0", 115200);
+        Log.d(TAG, "Mandado a Arduino: H");
+        uart.escribir("H");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e){
+            Log.w(TAG, "Error en sleep()", e);
+        }
+        String s = uart.leer();
+        Log.d(TAG, "Recibido de Arduino: "+s);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
